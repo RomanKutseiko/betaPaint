c13abb032bdeb5f2efdc2c31c73c270a91613593
Roman <roman.kutseiko@gmail.com>
2015-04-03 23:12:17 +0200

    Almost working drawing

--- Laba_2/src/FXApp/Controller.java
+++ Laba_2/src/FXApp/Controller.java
@@ -31,6 +31,52 @@ public class Controller {
 	}
 	
 	@FXML
+	private void btnTriangleOnClick() {
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.setFill(Color.RED);
+		double[] pointsX = new double[3]; //arrays are needed to create Polygons
+		double[] pointsY = new double[3];
+		pointsX[0] = getX(); 
+		pointsX[1] = getX()+(getWidth()/2);
+		pointsX[2] = getX()+getWidth();
+		pointsY[0] = getY()+ getHeight(); 
+		pointsY[1] = getY();
+		pointsY[2] = getX()+getHeight();
+		gc.fillPolygon(pointsX, pointsY, 3); //3 (points), Triangle
+	}
+	
+	@FXML
+	private void btnRectangleOnClick() {
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.setFill(Color.RED);
+		gc.fillRect(getX(), getY(), getWidth(), getHeight());
+	}
+	
+	@FXML
+	private void btnLineOnClick() {
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.setFill(Color.RED);
+		gc.strokeLine(getX(), getY(), getWidth(), getHeight());
+	}
+	
+	@FXML
+	private void btnParallelogramOnClick() {
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.setFill(Color.RED);
+		double[] pointsX = new double[4]; //arrays are needed to create Polygons
+		double[] pointsY = new double[4];
+		pointsX[0] = getX(); 
+		pointsX[1] = getX()+(getHeight()/2);
+		pointsX[2] = getX()+getWidth();
+		pointsX[3] = getX()-(getHeight()/2);
+		pointsY[0] = getY(); 
+		pointsY[1] = getY()+getHeight();
+		pointsY[2] = getY()+getHeight();
+		pointsY[3] = getY();
+		gc.fillPolygon(pointsX, pointsY, 4); //4 (points)
+	}
+	
+	@FXML
 	private void btnConst(){
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		gc.setFill(Color.RED);

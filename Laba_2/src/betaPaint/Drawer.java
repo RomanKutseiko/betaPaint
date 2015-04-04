8b1cbe22923755d92bf579db6b976c19be390eae
Roman <roman.kutseiko@gmail.com>
2015-04-04 18:04:10 +0200

    add enter errors chek

---
+++ Laba_2/src/betaPaint/Drawer.java
@@ -0,0 +1,44 @@
+package betaPaint;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class Drawer {
+	
+	public static void Draw(Ellipse ellipse, GraphicsContext gc){		
+		gc.fillOval(ellipse.getX(), ellipse.getY(), ellipse.getWidth(), ellipse.getHeight());
+	}
+
+	public static void Draw(Line line, GraphicsContext gc){		
+		gc.strokeLine(line.getX(), line.getY(), line.getWidth(), line.getHeight());
+	}
+
+	public static void Draw(Rectangle rect, GraphicsContext gc){		
+		gc.fillRect(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
+	}
+	
+	public static void Draw(Triangle triangle, GraphicsContext gc){		
+		double[] pointsX = new double[3]; //arrays are needed to create Polygons
+		double[] pointsY = new double[3];
+		pointsX[0] = triangle.getX(); 
+		pointsX[1] = triangle.getX()+(triangle.getWidth()/2);
+		pointsX[2] = triangle.getX()+triangle.getWidth();
+		pointsY[0] = triangle.getY()+ triangle.getHeight(); 
+		pointsY[1] = triangle.getY();
+		pointsY[2] = triangle.getY()+triangle.getHeight();
+		gc.fillPolygon(pointsX, pointsY, 3); //3 (points), Triangle
+	}
+	
+	public static void Draw(Parallelogram parallelogram, GraphicsContext gc){		
+		double[] pointsX = new double[4]; //arrays are needed to create Polygons
+		double[] pointsY = new double[4];
+		pointsX[0] = parallelogram.getX(); 
+		pointsX[1] = parallelogram.getX()+(parallelogram.getWidth()/5);
+		pointsX[2] = parallelogram.getX()+parallelogram.getWidth();
+		pointsX[3] = parallelogram.getX()+parallelogram.getWidth()*4/5;
+		pointsY[0] = parallelogram.getY(); 
+		pointsY[1] = parallelogram.getY()+parallelogram.getHeight();
+		pointsY[2] = parallelogram.getY()+parallelogram.getHeight();
+		pointsY[3] = parallelogram.getY();
+		gc.fillPolygon(pointsX, pointsY, 4); //4 (points)
+	}
+}
